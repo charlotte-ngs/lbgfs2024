2bf2fb17f075a84a48c087b4648e8d470eade737
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2024-11-09 07:24:22 +0100

    Commit bash/deploy_material.sh

--- bash/deploy_material.sh
+++ bash/deploy_material.sh
@@ -55,15 +55,15 @@ REAL_EVALROOT=$(dirname $REAL_EVALREPO)
 usage () {
   local l_MSG=$1
   echo "Message: $l_MSG"
-  echo "Usage:   $SCRIPT -d <deploy_date> -h -l <link_text> -s <src_dir> -t <target_deploy_dir> -w <data_table_path> -z"
+  echo "Usage:   $SCRIPT -d <deploy_date> -h -l <link_text> -s <src_mat> -t <target_deploy_dir> -w <data_table_path> -z"
   echo '  where '
   echo '        -d <deploy_date>       --  (optional) alternative deployment date ...'
   echo '                                              > default: today ...'
   echo '        -h                     --  (optional) show usage message ...'
-  echo '        -l <link_text>.        --             text shown on website ...'
-  echo '        -s <src_dir>           --             directory from where material is deployed ...'
+  echo '        -l <link_text>         --             text shown on website ...'
+  echo '        -s <src_mat>           --             material consisting of files or directories to be deployed ...'
   echo '        -t <target_deploy_dir> --             target directory for deployment ...'
-  echo '        -w <data_table_path>.  --             path to data table file ...'
+  echo '        -w <data_table_path>   --             path to data table file ...'
   echo '        -z                     --  (optional) produce verbose output'
   echo '                                              > default: false'
   echo 
@@ -102,17 +102,52 @@ log_msg () {
   echo "[${l_RIGHTNOW} -- ${l_CALLER}] $l_MSG"
 }
 
+#' ### Move Material
+#' Specified material is moved from source to target
+#+ mv-mat-src-trg-fun
+move_mat_from_src_to_trg () {
+  for m in $SRC_MAT;do
+    if [[ $VERBOSE == 'true' ]];then log_msg move_mat_from_src_to_trg " * Move $m to $TRG_DPL_DIR ..."
+    mv $m $TRG_DPL_DIR
+  done
+}
+
 
 #' ## Getopts for Commandline Argument Parsing
 #' If an option should be followed by an argument, it should be followed by a ":".
 #' Notice there is no ":" after "h". The leading ":" suppresses error messages from
 #' getopts. This is required to get my unrecognized option code to work.
 #+ getopts-parsing, eval=FALSE
+SRC_MAT=''
+SRC_CP_FILE=''
+TRG_DPL_DIR=''
+DEPLOY_DATE=''
+LINK_TITLE=''
+DATA_TABLE_PATH=''
 VERBOSE='false'
-while getopts ":hz" FLAG; do
+while getopts ":d:hl:s:w:z" FLAG; do
   case $FLAG in
     h)
       usage "Help message for $SCRIPT"
+      ;;
+    d)
+      DEPLOY_DATE=$OPTARG
+      ;;
+    l)
+      LINK_TITLE=$OPTARG
+      ;;
+    s)
+      if [[ -d $OPTARG ]] || [[ -f $OPTARG ]];then
+        SRC_MAT=$OPTARG
+      else
+        usage " *** ERROR: CANNOT FIND source material to be deployed at: $OPTARG ..."
+      fi
+      ;;
+    t)
+      TRG_DPL_DIR=$OPTARG
+      ;;
+    w)
+      DATA_TABLE_PATH=$OPTARG
       ;;
     z)
       VERBOSE='true'
@@ -141,12 +176,43 @@ start_msg
 #' meaningful default value
 #+ argument-test, eval=FALSE
 log_msg $SCRIPT " * Check arguments and set defaults ..."
+if [[ $SRC_MAT == '' ]] ;then
+  usage " *** ERROR: -s <source_mat> required but not defined ..."
+fi
+if [[ $LINK_TITLE == '' ]];then
+  usage " *** ERROR: -l <link_title> required, but not defined ..."
+fi
+if [[ $DATA_TABLE_PATH == '' ]];then
+  usage " *** ERROR: -w <data_table_path> required, but not defined ..."
+fi
+if [[ $TRG_DPL_DIR == '' ]];then
+  usage " *** ERROR: -t <target_deploy_dir> required, but not defined ..."
+fi
+# create target, if it does not yet exist
+if [[ ! -d $TRG_DPL_DIR ]];then
+  if [[ $VERBOSE == 'true' ]];then log_msg $SCRIPT " * Create dir: $TRG_DPL_DIR ...";fi
+  mkdir -p $TRG_DPL_DIR
+fi
+# default for deploy date. is today
+if [[ $DEPLOY_DATE == '' ]];then
+  DEPLOY_DATE=$(date +"%Y-%m-%d")
+fi
+if [[ $VERBOSE == 'true' ]];then
+  log_msg $SCRIPT " * Check args and defaults ..."
+  log_msg $SCRIPT " * ==>  SRC_MAT:          $SRC_MAT ..."
+  log_msg $SCRIPT " * ==>  SRC_CP_FILE:      $SRC_CP_FILE ..."
+  log_msg $SCRIPT " * ==>  TRG_DPL_DIR:      $TRG_DPL_DIR ..."
+  log_msg $SCRIPT " * ==>  LINK_TITLE:       $LINK_TITLE ..."
+  log_msg $SCRIPT " * ==>  DEPLOY_DATE:      $DEPLOY_DATE ..."
+  log_msg $SCRIPT " * ==>  DATA_TABLE_PATH:  $DATA_TABLE_PATH ..."
+fi
 
 
-
-#' ## Your Code
-#' Continue to put your code here
-#+ your-code-here
+#' ## Move Material from Source To Target
+#' Material specified is moved from source to target
+#+ move-src-to_trg
+log_msg $SCRIPT " * Move material ..."
+move_mat_from_src_to_trg
 
 
 
